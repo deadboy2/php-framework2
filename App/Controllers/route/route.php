a7ba305c3e9358b9268d859d7aa4b3b4473f597b
deadboy2 <mihailhrustalev666@gmail.com>
2017-06-20 11:07:50 +0300

    update framework

--- App/Controllers/route/route.php
+++ App/Controllers/route/route.php
@@ -11,17 +11,6 @@ namespace App\Controllers\route;
 // Получаем текущий полный URL
 $url = parse_url("https://".$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']);
 
-// Распетрушиваем путь на «папки»
-$dirs = explode('/', $url['path']);
-
-// Парсим переменные GET в глобальный массив $_GET
-isset($url['query']) && parse_str($url['query'], $_GET);
-
-// Декодируем в UTF-8 все символы, отличные от латиницы
-for ($i=1; $i<(count($dirs)-1); $i++) {
-    $dirs[$i]=urldecode($dirs[$i]);
-}
-
 
 $mainController = new \App\Controllers\Main();
 $seoController = new \App\Controllers\Seo();
